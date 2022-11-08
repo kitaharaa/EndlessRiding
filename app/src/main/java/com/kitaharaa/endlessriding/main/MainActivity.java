fe9f2734f41568d01ee571029dfea3b181d63763
amogus3000 <bvalentin1415@gmail.com>
2022-11-08 20:10:08 +0200

    initial commit

---
+++ app/src/main/java/com/kitaharaa/endlessriding/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kitaharaa.endlessriding.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kitaharaa.endlessriding.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
