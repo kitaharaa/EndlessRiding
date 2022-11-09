9b848cf05ff7a350bc70ddbd0aac50a7be735068
amogus3000 <bvalentin1415@gmail.com>
2022-11-09 11:18:09 +0200

    added 2 types of activity_main.xml and splash screen

--- app/src/main/java/com/kitaharaa/endlessriding/main/MainActivity.java
+++ app/src/main/java/com/kitaharaa/endlessriding/main/MainActivity.java
@@ -1,15 +1,29 @@
 package com.kitaharaa.endlessriding.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.kitaharaa.endlessriding.R;
 
 public class MainActivity extends AppCompatActivity {
+    Boolean GAME_PASS = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        SplashScreen.installSplashScreen(this);
+
+        if (GAME_PASS) {
+            setContentView(R.layout.activity_main_web_view);
+            WebView webView = findViewById(R.id.webView);
+            webView.loadUrl("https://tinypng.com/");
+        } else {
+            setContentView(R.layout.activity_main);
+            getSupportActionBar().hide();
+        }
     }
 }
